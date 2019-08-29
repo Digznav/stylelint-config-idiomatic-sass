6e01205ca25bad3ba2b7a49206abd8661929888f
Diego Miguel <digznav@gmail.com>
2019-08-29 15:40:22 -0500

    Fixes

--- package-scripts.js
+++ package-scripts.js
@@ -2,28 +2,18 @@ const npsUtils = require('nps-utils'); // eslint-disable-line import/no-extraneo
 
 const serialize = npsUtils.series;
 
-const localBins = {
-  stylelint: './node_modules/.bin/stylelint --syntax scss "**/*.scss"',
-  prettier: './node_modules/.bin/prettier --write',
-  eslint: './node_modules/.bin/eslint "**/*.js"'
-};
+const prettier = 'prettier --write';
+const eslint = 'eslint "**/*.js"';
 
 module.exports = {
   scripts: {
     test: 'mocha',
     js: {
-      format: `${localBins.prettier} --single-quote --print-width=140 --parser=flow "**/*.js"`,
+      format: `${prettier} --single-quote --print-width=140 --parser=flow "**/*.js"`,
       lint: {
-        default: `${localBins.eslint} || true`,
-        fix: `${localBins.eslint} --fix`,
-        strict: localBins.eslint
-      }
-    },
-    sass: {
-      format: `${localBins.stylelint} --fix`,
-      lint: {
-        default: `${localBins.stylelint} || true`,
-        strict: `${localBins.stylelint}`
+        default: `${eslint} || true`,
+        fix: `${eslint} --fix`,
+        strict: eslint
       }
     },
     link: serialize(serialize.nps('js.lint.strict'), 'npm link'),
